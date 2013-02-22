88113dda136014d45dc4002b293ffe050227f232
MichalM <michal.j.musial@gmail.com>
2013-02-22 09:43:46 +0100

    corrected class name {form -> Form}

--- views/devtools/route-test.php
+++ views/devtools/route-test.php
@@ -4,22 +4,22 @@
 		background:#eee;
 		border:2px solid #ccc;
 	}
-	
+
 	table.route-test th {
 		background:#ddd;
 		padding:5px;
 	}
-	
+
 	table.route-test td {
 		padding:2px 10px;
 	}
-	
+
 	table.route-test .error {
 		background:#FFBBBB;
 		color:red;
 		font-weight:bold;
 	}
-	
+
 	table.route-test .pass {
 		background:#BBFFBB;
 		color:green;
@@ -32,7 +32,7 @@
 	<?php echo Form::open() ?>
 		<label>Test the url:</label>
 		<?php echo Form::input('url',arr::get($_POST,'url'),array('style'=>'width:300px')) ?>
-		<?php echo form::submit('test','Test'); ?>
+		<?php echo Form::submit('test','Test'); ?>
 	<?php echo Form::close() ?>
 </p>
 
@@ -47,7 +47,7 @@
 	'another/url',
 	'guide/api/Class',
 	'guide/media/image.png',
-	
+
 	// Test that these uris returns the following route and params
 	'guide/media/image.png' = array(
 		'route' => 'docs/media',
@@ -69,51 +69,51 @@
 <?php else: ?>
 
 	<?php foreach ($tests as $test) :?>
-	
+
 	<table class="route-test">
 		<tr><th colspan="3">Testing the url "<code><?php echo $test->url ?></code>"</th></tr>
-		
+
 		<?php if ($test->route === FALSE): ?>
-		
+
 			<tr><td colspan="3" class="error">Did not match any routes</td></tr>
-		
+
 		<?php else:?>
-		
+
 			<?php if ($test->expected_params): ?>
-				
+
 				<tr><th>param</th><th>result</th><th>expected</th>
-			
+
 				<?php
 				foreach ($test->get_params() as $name => $param)
 				{
 					echo "<tr><td>{$name}</td><td".($param['error'] ? ' class="error"':' class="pass"').">{$param['result']}</td><td".($param['error'] ? ' class="error"':' class="pass"').">{$param['expected']}</td>";
 				}
 				?>
-				
+
 			<?php else: ?>
-			
+
 				<?php foreach ($test->params as $key => $value): ?>
 					<tr><td><?php echo $key ?>:</td><td colspan="2"><?php echo $value ?></td></tr>
 				<?php endforeach; ?>
-				
+
 			<?php endif; ?>
-			
+
 		<?php endif; ?>
-		
+
 	</table>
-	
+
 	<?php endforeach ?>
-	
-	
+
+
 	<h2>Copy/paste friendly version:</h2>
-	
+
 	<pre style="border:1px dashed #666;padding:10px;" ><?php
-	
+
 	foreach ($tests as $test)
 	{
-	
+
 		echo "Testing the url \"{$test->url}\"\n";
-		
+
 		if ($test->route === FALSE)
 		{
 			echo " ! Did not match any routes\n";
@@ -126,7 +126,7 @@
 				{
 					echo ($param['error'] ? ' ✓ ' : ' ! ' ).str_pad(str_pad($name.': ',15).$param['result'].' ',35).'(expecting '.$param['expected'].")\n";
 				}
-				
+
 			}
 			else
 			{
