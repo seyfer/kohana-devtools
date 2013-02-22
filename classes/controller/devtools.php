7584657d914f227cb60eed2a695fb4796d8de5ee
MichalM <michal.j.musial@gmail.com>
2013-02-22 09:41:43 +0100

    updated config loading to Kohana 3.2

--- classes/controller/devtools.php
+++ classes/controller/devtools.php
@@ -8,30 +8,30 @@ class Controller_devtools extends Controller {
 	{
 		$this->template = View::factory('devtools/template');
 	}
-	
+
 	public function after()
 	{
 		$this->response->body($this->template->render());
 	}
 
 	/**
-	 * Dump constants, Kohana::init() settings, loaded modules, and install.php 
+	 * Dump constants, Kohana::init() settings, loaded modules, and install.php
 	 */
 	public function action_info()
 	{
 		$this->template->content = View::factory('devtools/info');
 	}
-	
+
 	/**
 	 * Show all classes that are getting transparent extended
 	 */
 	public function action_extension()
 	{
 		$classes = Arr::flatten(Kohana::list_files('classes'));
-	
+
 		$this->template->content = View::factory('devtools/extension',array('classes'=>$classes));
 	}
-	
+
 	/**
 	 * Test a route
 	 */
@@ -39,18 +39,18 @@ class Controller_devtools extends Controller {
 	{
 		// Check if a url was provided
 		$url = Arr::get($_POST,'url',FALSE);
-		
+
 		if ( ! $url)
 		{
 			// Try to find a config file
-			$url = Kohana::config('route-test');
+			$url = Kohana::$config->load('route-test');
 		}
 		$this->template->content = View::factory('devtools/route-test',array(
 			// Get all the tests
 			'tests' => Route_Tester::create_tests($url),
 		));
 	}
-	
+
 	/**
 	 * Dump all routes
 	 */
@@ -58,31 +58,31 @@ class Controller_devtools extends Controller {
 	{
 		$this->template->content = View::factory('devtools/route-dump');
 	}
-	
+
 	/**
 	 * Dump all config files
 	 */
 	public function action_config()
 	{
 		$files = Kohana::list_files('config');
-		
+
 		$configs = array();
 		foreach($files as $key => $value)
 		{
 			// Trim off "config/" and ".php"
 			$configs[$key] = substr($key,7,-strlen(EXT));
 		}
-		
+
 		$this->template->content = View::factory('devtools/config',array('configs'=>$configs));
 	}
-	
+
 	/**
 	 * Dump all message files
 	 */
 	public function action_message()
 	{
 		$files = Kohana::list_files('messages');
-		
+
 		$messages = array();
 		foreach($files as $key => $value)
 		{
@@ -91,14 +91,14 @@ class Controller_devtools extends Controller {
 		}
 		$this->template->content = View::factory('devtools/message',array('messages'=>$messages));
 	}
-	
+
 	/**
 	 * Dump all i18n files
 	 */
 	public function action_i18n()
 	{
 		$files = Kohana::list_files('i18n');
-		
+
 		$i18n = array();
 		foreach($files as $key => $value)
 		{
